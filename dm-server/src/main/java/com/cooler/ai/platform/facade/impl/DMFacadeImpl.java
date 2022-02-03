6581a676c9048931cc50d0ca958172f810c288ae
cooler130 <zzss_2003@163.com>
2022-02-04 00:17:56 +0800

    开始v3.0

--- dm-server/src/main/java/com/cooler/ai/platform/facade/impl/DMFacadeImpl.java
+++ dm-server/src/main/java/com/cooler/ai/platform/facade/impl/DMFacadeImpl.java
@@ -1,6 +1,7 @@
 package com.cooler.ai.platform.facade.impl;
 
 import com.cooler.ai.platform.entity.Action;
+import com.cooler.ai.platform.facade.constance.Constant;
 import com.cooler.ai.platform.facade.model.*;
 import com.cooler.ai.platform.service.framework.*;
 import com.cooler.ai.platform.facade.DMFacade;
@@ -45,26 +46,32 @@ public class DMFacadeImpl implements DMFacade {
         Map<String, BizDataModelState<String>> bizDataMSMap = distributionData.getBizDataMSMap();
         Action startAction = null;
         DMResponse dmResponse = null;
+        String debugModel = dmRequest.isDebugModel();
+        debugModel = (debugModel == null ? Constant.MODEL_RDB : debugModel);
 
-        if(!dmRequest.isDebugModel()){                                                                                  //非调试状态，后端接入的是json文件内部数据（用于正式环境）
+        if(debugModel.equals(Constant.MODEL_KVDB)){                                                                                  //KV模式，通常是线上状态，后端读入json文件内部数据
 
-            dstService.fsmDSTProcess(dmRequest, dialogState, bizDataMSMap);                                                               //  3.状态检测、迁移过程追踪（DST过程），检测各个槽位的状态，以及迁移过程，得到最终状态
+            dstService.fsmDSTProcess(dmRequest, dialogState, bizDataMSMap);                                             //  3.状态检测、迁移过程追踪（DST过程），检测各个槽位的状态，以及迁移过程，得到最终状态
 
-            startAction = policyProcessService.queryPolicy(dialogState);                                                    //  4.根据策略，查询动作
+            startAction = policyProcessService.queryPolicy(dialogState);                                                //  4.根据策略，查询动作
 
-            dmResponse = policyProcessService.runActions(dmRequest, dialogState, startAction, bizDataMSMap);                //  5.执行动作（globalMap会记录中间产生的链式动作集合），这个过程bizDataMSMap会新增各个业务参数，它是一个增量集合
+            dmResponse = policyProcessService.runActions(dmRequest, dialogState, startAction, bizDataMSMap);            //  5.执行动作（globalMap会记录中间产生的链式动作集合），这个过程bizDataMSMap会新增各个业务参数，它是一个增量集合
 
-        } else {                                                                                                        //调试状态，后端接入的是数据库中的数据（用于线下测试环境和管理环境）
+        } else if(debugModel.equals(Constant.MODEL_RDB)){                                                                            //调试状态，后端接入的是数据库中的数据（用于线下测试环境和管理环境）
 
-            dstService2.fsmDSTProcess(dmRequest, dialogState, bizDataMSMap);                  //todo:还是要在这里输入bizDataMSMap，在各个dataTaskAction中也设计到业务数据的修改了
+            dstService2.fsmDSTProcess(dmRequest, dialogState, bizDataMSMap);
 
             startAction = policyProcessService2.queryPolicy(dialogState);
 
             dmResponse = policyProcessService2.runActions(dmRequest, dialogState, startAction, bizDataMSMap);
 
+        } else if(debugModel.equals(Constant.MODEL_GDB)){                                                                           //创新方式1尝试
+            //todo:图数据库模式
+        } else if(debugModel.equals(Constant.MODEL_TREE)){                                                                          //创新方式2尝试
+            //todo：树数据模式
         }
 
-        dataStoreService.storeData(dmRequest, dialogState, bizDataMSMap);                                                              //6.保存数据，下一轮需要的DialogState（同步进行）
+        dataStoreService.storeData(dmRequest, dialogState, bizDataMSMap);                                               //  6.保存数据，下一轮需要的DialogState（同步进行）
 
         long endTimeStamp = System.currentTimeMillis();
 
